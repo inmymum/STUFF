3b710bec4ce9923c3a8aab069e9ca0f6d8918372
inmymum <114446605+inmymum@users.noreply.github.com>
2023-10-06 01:31:36 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/inmymum/RadioSilence/main/Lib/UI/main.lua"))()
 local Window = Library.CreateLib("FuckerGUI", "DarkTheme")
+
+local LocalP = game.Players.LocalPlayer
+local Mouse = LocalP:GetMouse()
 function getPlayerNames() -- Get list of players for selection dropdown
     local playerNames = {}
     for _, player in pairs(game.Players:GetPlayers()) do
@@ -24,4 +27,11 @@ end)
 rape:NewButton("Fuck the bitch", "", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/inmymum/RadioSilence/main/Lib/Scripts/rapefunc.lua"))()
     fun(rapist, victim)
+end)
+rape:NewButton("RESET", "Reset your character to stop fucking her", function()
+    Mouse.KeyDown:Connect(function(key)
+        if key == "x" then
+            LocalP.Character.Torso:Destroy()
+        end
+    end)
 end)
