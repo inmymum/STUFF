f49bfbbc46496ddcb268fcadf4cd8ffbfc20c921
inmymum <114446605+inmymum@users.noreply.github.com>
2023-10-06 01:45:05 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,7 +11,6 @@ function getPlayerNames() -- Get list of players for selection dropdown
 end
 local movehub = Window:NewTab("Movement")
 local movement = movehub:NewSection("")
-
 local rapetab = Window:NewTab("Rape")
 local rape = rapetab:NewSection("Rape The hoes")
 local dropdown = rape:NewDropdown("Rapist","", getPlayerNames(), function(x)
